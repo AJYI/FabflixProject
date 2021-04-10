7f968c21c88924fa7583489fed20f6615291e292
ewen4736 <lizziebeth179@gmail.com>
2021-04-10 15:18:02 -0700

    Replaced the old starServlet with new one

--- src/StarsServlet.java
+++ src/StarsServlet.java
@@ -12,16 +12,15 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-
-// Declaring a WebServlet called StarsServlet, which maps to url "/api/stars"
-@WebServlet(name = "StarsServlet", urlPatterns = "/api/stars")
+@WebServlet(name = "StarsServlet", urlPatterns = "/api/star")
 public class StarsServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
 
-    // Create a dataSource which registered in web.
+    // Create a dataSource which registered in web.xmls
     private DataSource dataSource;
 
     public void init(ServletConfig config) {
@@ -33,42 +32,53 @@ public class StarsServlet extends HttpServlet {
     }
 
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         response.setContentType("application/json"); // Response mime type
+
+        // Retrieve parameter id from url request.
+        String id = request.getParameter("id");
 
         // Output stream to STDOUT
         PrintWriter out = response.getWriter();
 
         // Get a connection from dataSource and let resource manager close the connection after usage.
         try (Connection conn = dataSource.getConnection()) {
+            // Get a connection from dataSource
+
+            String query = "select s.name as 'starName',\n" +
+                    "        s.birthYear as 'birthYear',\n" +
+                    "        group_concat(distinct m.id order by m.title) 'movieID',\n" +
+                    "        group_concat(distinct m.title order by m.title) 'movies'\n" +
+                    "        from stars s, stars_in_movies sim, movies m\n" +
+                    "        where s.id = '" + id +"' AND s.id = sim.starId AND m.id = sim.movieId";
 
             // Declare our statement
             Statement statement = conn.createStatement();
-
-            String query = "SELECT * from stars";
 
             // Perform the query
             ResultSet rs = statement.executeQuery(query);
 
             JsonArray jsonArray = new JsonArray();
+            rs.next();
 
-            // Iterate through each row of rs
-            while (rs.next()) {
-                String star_id = rs.getString("id");
-                String star_name = rs.getString("name");
-                String star_dob = rs.getString("birthYear");
+            String starName = rs.getString("starName");
+            String birthYear = rs.getString("birthYear");
+            String movieID = rs.getString("movieID");
+            String movies = rs.getString("movies");
 
-                // Create a JsonObject based on the data we retrieve from rs
-                JsonObject jsonObject = new JsonObject();
-                jsonObject.addProperty("star_id", star_id);
-                jsonObject.addProperty("star_name", star_name);
-                jsonObject.addProperty("star_dob", star_dob);
+            // Create a JsonObject based on the data we retrieve from rs
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("star_name", starName);
+            jsonObject.addProperty("star_birth_year", birthYear);
+            jsonObject.addProperty("movie_id", movieID);
+            jsonObject.addProperty("movies", movies);
 
-                jsonArray.add(jsonObject);
-            }
+            jsonArray.add(jsonObject);
+
             rs.close();
             statement.close();
 
@@ -77,9 +87,7 @@ public class StarsServlet extends HttpServlet {
             // set response status to 200 (OK)
             response.setStatus(200);
 
-
         } catch (Exception e) {
-
             // write error message JSON object to output
             JsonObject jsonObject = new JsonObject();
             jsonObject.addProperty("errorMessage", e.getMessage());
@@ -90,7 +98,8 @@ public class StarsServlet extends HttpServlet {
         } finally {
             out.close();
         }
-        // always remember to close db connection after usage. Here it's done by try-with-resources
 
+        // always remember to close db connection after usage. Here it's done by try-with-resources
     }
+
 }
