25ad7b261aee75dce59223013d32c1c3db8436ac
ewen4736 <lizziebeth179@gmail.com>
2021-04-26 19:11:00 -0700

    Updated User class to hold more attributes

--- src/User.java
+++ src/User.java
@@ -2,12 +2,39 @@
  * This User class only has the username field in this example.
  * You can add more attributes such as the user's shopping cart items.
  */
+import java.util.ArrayList;
+
 public class User {
 
     private final String username;
+    public ArrayList<String> cartItems;
+    private double totalPrice;
+    private ShoppingCart cart;
 
     public User(String username) {
         this.username = username;
+        cart = new ShoppingCart();
+        totalPrice = 0;
     }
+
+
+    void showCart() {
+        // Transfer all movies and prices into an arrayList
+
+    }
+
+    int getMovieQuantity(String movie) {
+        return cart.getNumOfCopies(movie);
+    }
+
+    double getTotalMoviePrice(String movie) {
+        return cart.getMoviePriceSum(movie);
+    }
+
+    double getTotal() {
+        totalPrice = cart.getTotalCartPrice();
+        return totalPrice;
+    }
+
 
 }
