c2675d7405aaeed5c3a4ec072cb7871c4559dfec
ewen4736 <lizziebeth179@gmail.com>
2021-04-26 18:05:17 -0700

    Created Shopping Cart class

---
+++ src/ShoppingCart.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.text.*;
+import java.lang.*;
+
+public class ShoppingCart {
+    private int totalItems;
+    private double totalPrice;
+    private Map<String, Integer> shoppingCart;
+    private Map<String, Double> moviePrices;
+
+
+    ShoppingCart() {
+        shoppingCart = new HashMap<String, Integer>();
+        moviePrices = new HashMap<String, Integer>();
+        totalItems = 0;
+        totalPrice = 0;
+    }
+
+    void addToCart(String movie) {
+        // Check to see if item exists within the map
+        if(shoppingCart.containsKey(movie)) {
+            shoppingCart.put(movie, shoppingCart.get(movie) + 1);
+            totalItems++;
+        }
+        else {
+            shoppingCart.put(movie, 1);
+            totalItems++;
+            setMoviePrice(movie);
+        }
+    }
+
+    void removeMovieFromCart(String movie) {
+        if(shoppingCart.containsKey(movie)) {
+            totalItems -= shoppingCart.get(movie);
+            shoppingCart.remove(movie);
+            moviePrices.remove(movie);
+        }
+    }
+
+
+    void removeOneCopy(String movie) {
+        if(shoppingCart.containsKey(movie)) {
+            if(shoppingCart.get(movie) == 1) {
+                removeMovieFromCart(movie);
+                totalItems--;
+            }
+            else {
+                shoppingCart.put(movie, shoppingCart.get(movie) - 1);
+                totalItems--;
+            }
+        }
+    }
+
+    void addOneCopy(String movie) {
+            shoppingCart.put(movie, shoppingCart.get(movie) + 1);
+            totalItems++;
+    }
+
+    int returnTotalNumOfItems() {
+        return totalItems;
+    }
+
+    void setMoviePrice(String movie) {
+        Random rand = new Random();
+        double price = Math.round(rand.nextDouble() * 5) + 5;
+        moviePrices.put(movie, price);
+    }
+
+    double getTotalPrice() {
+        for (String movieTitle : shoppingCart.keySet()) {
+            totalPrice += shoppingCart.get(movieTitle) * moviePrices.get(movieTitle);
+        }
+
+        return totalPrice;
+    }
+
+
+}
+
+
